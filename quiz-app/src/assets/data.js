dec6ba0c05510018694dcd4f9d64dd505cf36db2
triaanandafh <fadiltria51@gmail.com>
2025-07-16 13:34:00 +0700

    push 2nd project

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        question: "Which device is required for the internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which continent has the highest number of countries?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question: "A Computer cannot BOOT if it does not have the?",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating systems",
+        option4: "Mouse",
+        ans: 3,
+    },
+    {
+        question: "First Page of website is termed as?",
+        option1: "Index page",
+        option2: "Homepage",
+        option3: "Sitemap",
+        option4: "Pen drive",
+        ans: 2,
+    },
+]
